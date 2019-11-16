154d7c637dfec213fba593459db5516a05a50744
shivanandan.l <shivktg@gmail.com>
2019-11-16 11:57:51 +0530

    Add files via upload

---
+++ invite/script.js
@@ -0,0 +1,37 @@
+$(function(){
+
+  if (!$('.envelope').hasClass('open')){
+    $('.envelope').click(function(){
+      $(this).removeClass('new').addClass('open');
+    });
+  }
+  
+  $('.mailform input, .mailform textarea').on('keyup', function(){
+    if(this.value !== ''){
+      $(this).prev('label').addClass('show');
+    } else {
+      $(this).prev('label').removeClass('show');
+    }
+  }).on("focus",function(){
+    $(this).prev("label").addClass('focus');
+  }).on("blur",function(){
+      $(this).prev("label").removeClass('focus');
+  });
+  
+  $('.notification').find('p').last().click(function(){
+   $(this).closest('.notification').prev('.envelope').removeClass('send').addClass('new');
+   $(this).closest('.notification').prev('.envelope').find('.mailform')[0].reset();
+    $(this).closest('.notification').prev('.envelope').find('label').removeClass('show');
+  });
+  
+  $('.mailform').submit(function(event) {
+    event.preventDefault();
+    
+    $(this).closest('.envelope').removeClass('open').addClass('send');
+
+    //Put jQuery code for submitting the form with AJAX here.
+    var url = "https://www.facebook.com/events/465032574222129/";
+    $(location).attr('href', url);
+  });
+  
+});
